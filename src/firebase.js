bfb4a609774e98f65ee275fb9570b7f56810f037
nine-chairs <havlik9@gmail.com>
2023-12-06 11:58:02 +0100

    updated gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,7 @@
-import { initializeApp } from "firebase/app";
-import {getStorage} from "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDyMa0zZDrNFXUvNNBOdDYJWC1PORC_6C0",
-  authDomain: "supersilla-9b878.firebaseapp.com",
-  projectId: "supersilla-9b878",
-  storageBucket: "supersilla-9b878.appspot.com",
-  messagingSenderId: "238266011230",
-  appId: "1:238266011230:web:cdafc758ce5a7bee166ecf",
-  measurementId: "G-S8QVBHQ5SD"
-};
+import { initializeApp } from "firebase/app"
+import { getStorage } from "firebase/storage"
+import { firebaseConfig } from "./firebaseConfig" 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
+const app = initializeApp(firebaseConfig)
+export const storage = getStorage(app)
